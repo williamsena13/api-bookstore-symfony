6016e50775029b243057a2cb8f51ce04ef700a7a
William B. Sena <williamsena13@gmail.com>
2026-04-15 10:33:51 -0300

    feat: adiciona charts nos relatórios, templates de edição, erros e atualiza usuários

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,6 +6,7 @@ use App\Repository\AssuntoRepository;
 use App\Repository\AutorRepository;
 use App\Repository\EditoraRepository;
 use App\Repository\LivroRepository;
+use App\Service\RelatorioService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,8 +21,8 @@ class HomeController extends AbstractController
         AssuntoRepository $assuntoRepository,
     ): Response {
         return $this->render('home/index.html.twig', [
-            'totalLivros' => $livroRepository->count([]),
-            'totalAutores' => $autorRepository->count([]),
+            'totalLivros'   => $livroRepository->count([]),
+            'totalAutores'  => $autorRepository->count([]),
             'totalEditoras' => $editoraRepository->count([]),
             'totalAssuntos' => $assuntoRepository->count([]),
         ]);
@@ -31,5 +32,73 @@ class HomeController extends AbstractController
     public function tech(): Response
     {
         return $this->render('home/tech.html.twig');
+    }
+
+    #[Route('/livros', name: 'app_public_livros')]
+    public function livros(LivroRepository $repo): Response
+    {
+        return $this->render('public/livros.html.twig', [
+            'livros' => $repo->findAllWithRelations(),
+        ]);
+    }
+
+    #[Route('/autores', name: 'app_public_autores')]
+    public function autores(AutorRepository $repo): Response
+    {
+        return $this->render('public/autores.html.twig', [
+            'autores' => $repo->findAllWithCount(),
+        ]);
+    }
+
+    #[Route('/editoras', name: 'app_public_editoras')]
+    public function editoras(EditoraRepository $repo): Response
+    {
+        return $this->render('public/editoras.html.twig', [
+            'editoras' => $repo->findAllWithCount(),
+        ]);
+    }
+
+    #[Route('/assuntos', name: 'app_public_assuntos')]
+    public function assuntos(AssuntoRepository $repo): Response
+    {
+        return $this->render('public/assuntos.html.twig', [
+            'assuntos' => $repo->findAllWithCount(),
+        ]);
+    }
+
+    #[Route('/relatorios', name: 'app_public_relatorios')]
+    public function relatorios(): Response
+    {
+        return $this->render('public/relatorios.html.twig');
+    }
+
+    #[Route('/relatorios/por-autor', name: 'app_public_relatorio_por_autor')]
+    public function relatoriosPorAutor(RelatorioService $service): Response
+    {
+        return $this->render('public/relatorio_por_autor.html.twig', [
+            'agrupado' => $service->getLivrosPorAutor(),
+        ]);
+    }
+
+    #[Route('/relatorios/por-editora', name: 'app_public_relatorio_por_editora')]
+    public function relatoriosPorEditora(RelatorioService $service): Response
+    {
+        return $this->render('public/relatorio_por_editora.html.twig', [
+            'agrupado' => $service->getLivrosPorEditora(),
+        ]);
+    }
+
+    #[Route('/relatorios/por-assunto', name: 'app_public_relatorio_por_assunto')]
+    public function relatoriosPorAssunto(RelatorioService $service): Response
+    {
+        return $this->render('public/relatorio_por_assunto.html.twig', [
+            'agrupado' => $service->getLivrosPorAssunto(),
+        ]);
+    }
+
+    #[Route('/relatorios/ranking', name: 'app_public_relatorio_ranking')]
+    public function relatoriosRanking(RelatorioService $service): Response
+    {
+        return $this->render('public/relatorio_ranking.html.twig', $service->getRankingData());
     }
 }
